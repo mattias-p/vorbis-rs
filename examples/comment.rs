d5b3956144b832feb3f93d39f0579e2a145eb970
Mattias Päivärinta <mattias.paivarinta@doxwork.com>
2016-03-20 02:19:52 +0100

    Better extraction of SPLICE

--- examples/comment.rs
+++ examples/comment.rs
@@ -4,6 +4,12 @@ extern crate vorbis;
 use std::error;
 use std::fs::File;
 use std::path::Path;
+use std::io::Write;
+use std::error::Error;
+
+fn warn(message: &str) {
+    writeln!(&mut std::io::stderr(), "Warning: {}", message).unwrap();
+}
 
 fn main() {
     // let path = Path::new("examples/Epoq-Lepidoptera.ogg");
@@ -20,10 +26,29 @@ fn main() {
 
     let decoder = vorbis::Decoder::new(file).unwrap();
     println!("{}", decoder.vendor().expect("vendor"));
-    for item in decoder.comments() {
-        match item {
-            Ok((key, value)) => println!("{} = {}", key, value),
-            Err(err) => println!("{}", err),
-        }
+
+    let splice = decoder.comments()
+                        .flat_map(|result| {
+                            result.map_err(|err| warn(err.description()))
+                                  .into_iter()
+                        })
+                        .flat_map(|key_value| {
+                            if key_value.0 == "SPLICE" {
+                                Some(key_value.1)
+                            } else {
+                                None
+                            }
+                        })
+                        .fold(None, |acc, value| {
+                            if acc.is_some() {
+                                warn("Multiple SPLICE comments encountered, using the last one.");
+                            }
+                            Some(value)
+                        });
+
+    if let Some(splice) = splice {
+        println!("SPLICE: {}", splice);
+    } else {
+        warn("No SPLICE Vorbis comment found.");
     }
 }
